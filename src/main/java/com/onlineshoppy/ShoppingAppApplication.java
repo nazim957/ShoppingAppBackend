d02ef2ed1cecdca63ca251131e6dc343f320ebbb
Mohd Nazim <mohdnazim957@gmail.com>
2023-08-14 17:03:02 +0530

    Updated Main

--- src/main/java/com/onlineshoppy/ShoppingAppApplication.java
+++ src/main/java/com/onlineshoppy/ShoppingAppApplication.java
@@ -1,5 +1,6 @@
 package com.onlineshoppy;
 
+import com.onlineshoppy.dao.UserRepository;
 import com.onlineshoppy.entity.user.Role;
 import com.onlineshoppy.entity.user.User;
 import com.onlineshoppy.entity.user.UserRole;
@@ -22,6 +23,9 @@ public class ShoppingAppApplication implements CommandLineRunner {
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+	@Autowired
+	private UserRepository userRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingAppApplication.class, args);
 	}
@@ -29,42 +33,35 @@ public class ShoppingAppApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-//		try {
-//
-//
-//
-////
-//			User user = new User();
-//
-//			user.setFirstName("Mohd");
-//			user.setLastName("Nazim");
-//			user.setUsername("nazim@gmail.com");
-//			user.setPassword(this.bCryptPasswordEncoder.encode("abc"));
-//			user.setEmail("nazim@@gmail.com");
-//
-//
-//			Role role1 = new Role();
-//			role1.setRoleId(44L);
-//			role1.setRoleName("ADMIN");
-//
-//			Set<UserRole> userRoleSet = new HashSet<>();
-//			UserRole userRole = new UserRole();
-//
-//			userRole.setRole(role1);
-//
-//			userRole.setUser(user);
-//
-//			userRoleSet.add(userRole);
-//
-//			User user1 = this.userService.createUser(user, userRoleSet);
-//			System.out.println(user1.getUsername());
-//
-//
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//
-//
-//		}
+
+		if(userRepository.findByUsername("admin@gmail.com")==null) {
+			User user = new User();
+
+			user.setFirstName("Mohd");
+			user.setLastName("Nazim");
+			user.setUsername("admin@gmail.com");
+			user.setPassword(this.bCryptPasswordEncoder.encode("admin@123"));
+			user.setEmail("admin@gmail.com");
+
+
+			Role role1 = new Role();
+			role1.setRoleId(44L);
+			role1.setRoleName("ADMIN");
+
+			Set<UserRole> userRoleSet = new HashSet<>();
+			UserRole userRole = new UserRole();
+
+			userRole.setRole(role1);
+
+			userRole.setUser(user);
+
+			userRoleSet.add(userRole);
+
+			User user1 = this.userService.createUser(user, userRoleSet);
+			System.out.println(user1.getUsername());
+
+
+		}
 
 		System.out.println("starting code");
 	}
